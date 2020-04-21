8f8d4b2029402df20a10219dd1142b3854abee46
Michel <michelschep@gmail.com>
2020-04-21 08:47:03 +0200

    Focus on one test

--- src/Bantumi/Bantumi.Tests.Acceptance/BantumiBoard.feature
+++ src/Bantumi/Bantumi.Tests.Acceptance/BantumiBoard.feature
@@ -12,27 +12,28 @@ Scenario: Play a non strike move
 	Given an initial bantumi board
 	When Michel plays field 4
 	Then the board should be 
-			| 4 | 4 | 4 | 4 | 4 | 5 |
+			| F1 | F2  | F3 | F4 | F5 | F6 |
+			| 4  | 4 | 4 | 4 | 4 | 5 |
 			| 4 | 4 | 4 | 0 | 5 | 5 |
 	And score Michel-Geale should be 1-0
 	And it is Geale's turn
 
-Scenario: Play a non strike move
-	Given an initial bantumi board
-	When Michel plays field 2
-	Then the board should be 
-			| 4 | 4 | 4 | 4 | 4 | 5 |
-			| 4 | 0 | 5 | 5 | 5 | 5 |
-	And score Michel-Geale should be 1-0
-	And it is Geale's turn
-
-Scenario: Play a strike move
-	Given an initial bantumi board
-	When Michel plays field 4
-	Then the board should be 
-			| 4 | 4 | 4 | 4 | 4 | 4 |
-			| 4 | 4 | 0 | 5 | 5 | 5 |
-	And score Michel-Geale should be 1-0
-	And it is Michel's turn
+#Scenario: Play a non strike move without scoring
+#	Given an initial bantumi board
+#	When Michel plays field 2
+#	Then the board should be 
+#			| 4 | 4 | 4 | 4 | 4 | 5 |
+#			| 4 | 0 | 5 | 5 | 5 | 5 |
+#	And score Michel-Geale should be 1-0
+#	And it is Geale's turn
+#
+#Scenario: Play a strike move
+#	Given an initial bantumi board
+#	When Michel plays field 4
+#	Then the board should be 
+#			| 4 | 4 | 4 | 4 | 4 | 4 |
+#			| 4 | 4 | 0 | 5 | 5 | 5 |
+#	And score Michel-Geale should be 1-0
+#	And it is Michel's turn
 
 
---
+++ src/Bantumi/Bantumi.Tests.Acceptance/BantumiBoardSteps.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Bantumi.Tests.Acceptance
+{
+    [Binding]
+    public class BantumiBoardSteps
+    {
+        [Given(@"a player named (.*)")]
+        public void GivenAPlayerNamed(string namePlayer)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"(.*) plays (.*)")]
+        public void GivenFirstPlayerPlaysScondPlayer(string firstPlayer, string secondPlayer)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+                [Given(@"an initial bantumi board")]
+        public void GivenAnInitialBantumiBoard()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Michel plays field (.*)")]
+        public void WhenMichelPlaysField(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the board should be")]
+        public void ThenTheBoardShouldBe(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"score Michel-Geale should be (.*)")]
+        public void ThenScoreMichel_GealeShouldBe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"it is Geale's turn")]
+        public void ThenItIsGealeSTurn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"it is Michel's turn")]
+        public void ThenItIsMichelSTurn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
